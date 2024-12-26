7ce2ff3ceaddddf0d1a8be040ef63daccfffabfe
brahmaiah <30312324+bramhi123@users.noreply.github.com>
2024-12-26 19:42:35 +0530

    Update kind-commands.sh

--- Kind-K8s-3/kind-commands.sh
+++ Kind-K8s-3/kind-commands.sh
@@ -1,9 +1,10 @@
 
 kind create cluster --name=multi-node-cluster --config=kind-config.yaml
 kind delete cluster --name multi-node-cluster
-
+kind get clusters
 kubectl cluster-info
 docker ps -a
+docker network ls
 docker exec -it kind-external-load-balancer sh  [ this command for control-plane 2 or 3 extra configuration ]
 ls
 ps -ef [ see output haproxy ]
